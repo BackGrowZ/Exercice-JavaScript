f1533c19651aea7ee84ae47d907179fa142edd43
BackGrowZ <anthony.carreta@gmail.com>
2020-01-23 14:20:02 +0100

    Mise en place des fichier dans des array

--- rectangle.js
+++ rectangle.js
@@ -1,22 +1,48 @@
 const fs = require('fs')
+
 var fil1 = process.argv[2];
 var fil2 = process.argv[3];
-get(fil1,fil2)
+
+read(fil1,fil2)
 
 
-function get(fil1,fil2){
-    var chemin = '/home/backgrowz/projects/Module-6/';
+function read(fil1,fil2){
     var chemin1 = '/home/backgrowz/projects/Module-6/'+ fil1;
     var chemin2 = '/home/backgrowz/projects/Module-6/'+ fil2;
-    console.log(chemin1)
-    console.log(chemin2)
     
-    fs.readFile(chemin + fil1, 'utf8', (err, data) => {
-        if (err) throw err;
-        file1 = data;
-        console.log(file1)
-    });
-    
-    //console.log(file1)
+    var file1 = fs.readFileSync(chemin1, "utf8");
+    var file2 = fs.readFileSync(chemin2, "utf8");
+    var index = [["0"],["0"]];
+    var c1_data = [[],[],[]];
+    var c2_data = [[],[],[],[],[]];
+    var arrayNb = 0;
+    var arrayNb2 = 0;
+
+    for (i=0; i<file1.length; i++){ // Mise en forme du c1 en tableau 
+        if (file1.charAt(i) == "\n"){
+            arrayNb++
+            arrayNb2=0;
+        } else {
+            c1_data[arrayNb][arrayNb2] = file1.charAt(i);
+            arrayNb2++
+        }
+        
+    }
+
+    arrayNb=0;
+    arrayNb2=0;
+
+    for (i=0; i<file2.length; i++){ // Mise en forme du c2 en tableau 
+        if (file2.charAt(i) == "\n"){
+            arrayNb++
+            arrayNb2=0;
+        } else {
+            c2_data[arrayNb][arrayNb2] = file2.charAt(i);
+            arrayNb2++
+        }
+        
+    }
+
+   
 }
 
