3897ea1a387cbafa2217f3b4af495006fc31e96c
BackGrowZ <32708861+BackGrowZ@users.noreply.github.com>
2020-01-26 17:24:46 +0100

    Update tri.js
    Clean code

--- tri.js
+++ tri.js
@@ -1,8 +1,8 @@
-var number1 = process.argv.slice(2);
-var number2 = process.argv.slice(3);
-var number3 = process.argv.slice(4);
-var number4 = process.argv.slice(5);
-var number5 = process.argv.slice(6);
+var number1 = process.argv[2];
+var number2 = process.argv[3];
+var number3 = process.argv[4];
+var number4 = process.argv[5];
+var number5 = process.argv[6];
 tri(number1,number2,number3,number4,number5)
 
 function tri(number1,number2,number3,number4,number5){
